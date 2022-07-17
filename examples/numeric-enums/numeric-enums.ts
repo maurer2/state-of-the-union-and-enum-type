0c8f6815d999056755efbe949586cdc133d82205
Martin <mr.mmaurer+work@gmail.com>
2022-07-17 23:32:12 +0100

    Improved numeric enum example

--- examples/numeric-enums/numeric-enums.ts
+++ examples/numeric-enums/numeric-enums.ts
@@ -1,67 +1,91 @@
-import HttpStatusCode from "./http-enums";
+import HttpStatusCode from './http-enums';
+
+type StatusCodeSuccess =
+  | HttpStatusCode.OK
+  | HttpStatusCode.CREATED
+  | HttpStatusCode.ACCEPTED
+  | HttpStatusCode.NON_AUTHORITATIVE_INFORMATION
+  | HttpStatusCode.NO_CONTENT
+  | HttpStatusCode.RESET_CONTENT
+  | HttpStatusCode.PARTIAL_CONTENT
+  | HttpStatusCode.MULTI_STATUS
+  | HttpStatusCode.ALREADY_REPORTED
+  | HttpStatusCode.IM_USED;
+
+// used to discriminate between types
+enum State {
+  ERROR = -1,
+  LOADING = 0,
+  SUCCESS = 1,
+}
 
 type LoadingState = {
-  state: "loading";
+  state: State.LOADING;
 };
 
 type ErrorState = {
-  state: "error";
+  state: State.ERROR;
   status: HttpStatusCode;
 };
 
 type SuccessState = {
-  state: "success";
-  status?: HttpStatusCode;
+  state: State.SUCCESS;
+  status?: StatusCodeSuccess;
   response: string;
 };
 
 const loading: LoadingState = {
-  state: "loading",
+  state: State.LOADING,
 };
 
 const error: ErrorState = {
-  state: "error",
+  state: State.ERROR,
   status: HttpStatusCode.BAD_REQUEST,
 };
 
 const success: SuccessState = {
-  state: "success",
+  state: State.SUCCESS,
   status: HttpStatusCode.OK,
-  response: "Task failed successfully",
+  response: 'Task failed successfully',
 };
 
 async function apiResponse(): Promise<LoadingState | ErrorState | SuccessState> {
   // https://github.com/lodash/lodash/blob/67389a8c78975d97505fa15aa79bec6397749807/lodash.js#L3875-L3886
-  const randomStateNumber: number = 0 + Math.floor(Math.random() * (2 - 0 + 1));
+  const randomStateNumber: number = Math.floor(Math.random() * 3);
 
-  return [
-    loading,
-    error,
-    success
-  ]?.at(randomStateNumber) ?? loading;
+  return [loading, error, success]?.at(randomStateNumber) ?? loading;
 }
 
+// https://learntypescript.dev/07/l8-discriminated-union
 async function fetchStuff(): Promise<void> {
   const currentState: LoadingState | ErrorState | SuccessState = await apiResponse();
 
   switch (currentState.state) {
-    case "loading": {
-      console.log(currentState.state);
+    case State.LOADING: {
+      console.log(`State: ${currentState.state}`);
       return;
     }
-    case "error": {
-      console.log(currentState.state);
-      console.log(currentState.status);
+    case State.ERROR: {
+      console.log(`State: ${currentState.state}`);
+      console.log(`Status: ${currentState.status}`);
       return;
     }
-    case "success": {
-      console.log(currentState.state);
-      console.log(currentState.status);
-      console.log(currentState.response);
+    case State.SUCCESS: {
+      console.log(`State: ${currentState.state}`);
+      console.log(`Status: ${currentState.status}`);
+      console.log(`Response: ${currentState.response}`);
+
+      // always false
+      // if (currentState.status === HttpStatusCode.LOCKED) {}
+
+      if (currentState.status !== HttpStatusCode.OK) {
+        console.warn("Something's fishy.");
+      }
+
       return;
     }
     default: {
-      console.log("default", currentState);
+      console.log('default', currentState);
     }
   }
 }
--- examples/syntactic-sugababes/index.ts
+++ examples/syntactic-sugababes/index.ts
@@ -1,47 +1,39 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 const members2 = {
-  Keisha: "Keisha",
-  Mutya: "Mutya",
-  Siobhán: "Siobhán",
-  Heidi: "Heidi",
-  Amelle: "Amelle",
-  Jade: "Jade",
+  Keisha: 'Keisha',
+  Mutya: 'Mutya',
+  Siobhán: 'Siobhán',
+  Heidi: 'Heidi',
+  Amelle: 'Amelle',
+  Jade: 'Jade',
 } as const;
-const members = [
-  "Keisha",
-  "Mutya",
-  "Siobhán",
-  "Heidi",
-  "Amelle",
-  "Jade",
-] as const;
+const members = ['Keisha', 'Mutya', 'Siobhán', 'Heidi', 'Amelle', 'Jade'] as const;
 type Members = typeof members[number];
 // type Members2 = keyof typeof members2;
 
 // Initial
-type LineUp1998 = Extract<Members, "Keisha" | "Mutya" | "Siobhán">; // no intellisense
+type LineUp1998 = Extract<Members, 'Keisha' | 'Mutya' | 'Siobhán'>; // no intellisense
 // or
-type LineUp1998_2 = Exclude<Members, "Heidi" | "Amelle" | "Jade">; // no intellisense
+type LineUp1998_2 = Exclude<Members, 'Heidi' | 'Amelle' | 'Jade'>; // no intellisense
 // or
-type LineUp1998_3 = keyof Pick<typeof members2, "Keisha" | "Mutya" | "Siobhán">;
+type LineUp1998_3 = keyof Pick<typeof members2, 'Keisha' | 'Mutya' | 'Siobhán'>;
 // or
-type LineUp1998_4 = keyof Omit<typeof members2, "Heidi" | "Amelle" | "Jade">;
+type LineUp1998_4 = keyof Omit<typeof members2, 'Heidi' | 'Amelle' | 'Jade'>;
 
-const members1998: ReadonlyArray<LineUp1998> = ["Keisha", "Mutya", "Siobhán"];
-
+const members1998: ReadonlyArray<LineUp1998> = ['Keisha', 'Mutya', 'Siobhán'];
 
 // Siobhán left / Heidi joined
-type LineUp2001 = Exclude<LineUp1998, "Siobhán"> | "Heidi";
+type LineUp2001 = Exclude<LineUp1998, 'Siobhán'> | 'Heidi';
 
-const members2001: ReadonlyArray<LineUp2001> = ["Keisha", "Mutya", "Heidi"];
-
+const members2001: ReadonlyArray<LineUp2001> = ['Keisha', 'Mutya', 'Heidi'];
 
 // Mutya left / Amelle joined
-type LineUp2005 = Exclude<LineUp2001, "Mutya"> | "Amelle";
+type LineUp2005 = Exclude<LineUp2001, 'Mutya'> | 'Amelle';
 
 const members2005: ReadonlyArray<LineUp2005> = ['Keisha', 'Heidi', 'Amelle'];
 
 // Keisha left/ Jade joined
-type LineUp2009 = Exclude<LineUp2005, "Keisha"> | "Jade";
+type LineUp2009 = Exclude<LineUp2005, 'Keisha'> | 'Jade';
 
 const members2009: ReadonlyArray<LineUp2009> = ['Heidi', 'Amelle', 'Jade'];
 
@@ -52,7 +44,6 @@ type LineUp2011 = Exclude<LineUp2009, LineUp2009> | LineUp1998;
 type LineUp2011_2 = Exclude<LineUp2009, 'Heidi' | 'Amelle' | 'Amelle'> | LineUp1998;
 
 const members2011: ReadonlyArray<LineUp2011> = ['Keisha', 'Mutya', 'Siobhán'];
-
 
 type remainingOriginalMembers2001 = LineUp1998 & LineUp2001;
 type remainingOriginalMembers2005 = LineUp1998 & LineUp2005;
